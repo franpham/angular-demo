b1898d7dd8de346aac13d9d2025763c6c18d7908
franpham <franpham@gmail.com>
2015-10-31 11:36:59 -1000

    step 3: created a ng-app

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// TO CREATE A MODULE:
+
+angular.module('myApp', []);    // 2nd arg is to import dependencies;
+
+var myApp = angular.module('myApp');
+
+myApp.config(function() {
+
+})
+.run(function() {
+
+});
