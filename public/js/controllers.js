7bd60a69dab869f5e9ce4e11b3ab95e05ac875fa
franpham <franpham@gmail.com>
2015-10-31 14:00:02 -1000

    created a Factory

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,9 +3,10 @@
 
 var myApp = angular.module('myApp');
 
-// $ = Angular's variable; no # = your variable;
-myApp.controller('myController', ['$scope', 'mainCharacter', function($scope, mainCharacter) {
+// $ = Angular's variable; no $ = your variable;
+myApp.controller('myController', ['$scope', 'mainCharacter', 'VersionFactory', function($scope, mainCharacter, VersionFactory) {
   $scope.myFirstName = 'Francis';
   $scope.myModel = 'Player 1 Ready!';
   $scope.mainCharacter = mainCharacter;
+  $scope.characterVersion = VersionFactory.characterVersion();
 }]);
---
+++ public/js/factories.js
@@ -0,0 +1,13 @@
+"use strict";
+// TO CREATE A FACTORY:
+
+angular.module('myApp')
+.factory('VersionFactory', ['APP_VERSION', 'mainCharacter', function(APP_VERSION, mainCharacter) {
+  return {
+    get: function() {
+      // and other functions to link to ORM;
+    },
+    characterVersion: function() {
+    return mainCharacter + ' ' + APP_VERSION;
+  }};
+}]);
